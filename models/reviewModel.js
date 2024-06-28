82cb0957e690afc38d6cee671a2b5899e2a0546b
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-06-28 18:20:20 +0400

    created reviewModel.js and add schema in it

---
+++ models/reviewModel.js
@@ -0,0 +1,49 @@
+// review / rating / createdAt / ref to tour / ref to user
+
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "Review can not be empty!"],
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Tour",
+        required: [true, "Review must belong to a tour."],
+      },
+    ],
+    user: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Users",
+        required: [true, "Review must belong to a tour."],
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// userSchema.pre("save", function (next) {
+//   if (!this.isModified("createdAt") || this.isNew) return next();
+//   this.createdAt = Date.now() - 1000;
+//   next();
+// });
+
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
