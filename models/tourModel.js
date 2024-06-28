f7cc67efb58dfecd062cb1ee75d250c01a931618
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-06-28 19:19:04 +0400

    implemented creat and get all revies full logic

--- models/tourModel.js
+++ models/tourModel.js
@@ -166,6 +166,7 @@ tourSchema.pre("aggregate", function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   console.log(this.pipeline());
   next();
+  Review;
 });
 
 // Create a model for the tour schema
