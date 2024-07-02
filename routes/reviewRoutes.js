1b3577fab6fdfa878b68a7ad80e5f737eaaa2320
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-07-02 15:37:00 +0400

    adding missing authentication and authorization logic in routers

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -8,11 +8,12 @@ const authController = require('../controllers/authController');
 // the reviews routes separately.
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview,
@@ -21,10 +22,12 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview,
+  )
   .delete(
-    // authController.protect,
-    // authController.restrictTo('admin', 'lead-guide'),
+    authController.restrictTo('user', 'admin'),
     reviewController.deleteReview,
   );
 
