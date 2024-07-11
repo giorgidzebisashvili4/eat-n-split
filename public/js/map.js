6d2d1561f19cf9cbcc894175e75b4bc61b52843e
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-07-11 20:31:42 +0400

    implemented map section

--- public/js/map.js
+++ public/js/map.js
@@ -5,7 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const [longStart, latStart] = locationsData[0].coordinates;
 
-  const map = L.map('map').setView([latStart, longStart], 8);
+  const map = L.map('map', {
+    scrollWheelZoom: false,
+  }).setView([latStart, longStart], 8);
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -13,17 +15,30 @@ document.addEventListener('DOMContentLoaded', () => {
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
 
-  for (i = locationsData.length - 1; i >= 0; i--) {
-    const currLocation = locationsData[i];
+  const bounds = [];
+  const markers = [];
 
+  for (let i = 0; i < locationsData.length; i++) {
+    const currLocation = locationsData[i];
     const [long, lat] = currLocation.coordinates;
+    bounds.push([lat, long]);
 
     const marker = L.marker([lat, long]).addTo(map);
 
-    marker
-      .bindPopup(
-        `<h1>Arrive on Day ${currLocation.day}</h1><br><h1>Location: ${currLocation.description}.</h1>`,
-      )
-      .openPopup();
+    marker.bindPopup(
+      `<h1>Arrive on Day ${currLocation.day}</h1><br><h1>Location: ${currLocation.description}.</h1>`,
+    );
+
+    markers.push(marker);
   }
+
+  // Fit the map to the bounds of all the locations with padding
+  map.fitBounds(bounds, { padding: [150, 150] });
+
+  // Center the zoom control
+  const zoomControl = document.querySelector('.leaflet-control-zoom');
+  zoomControl.style.marginTop = '200px';
+
+  // Open the popup of the first location
+  markers[0].openPopup();
 });
