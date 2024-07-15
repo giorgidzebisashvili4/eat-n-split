6ba6d55cfe49352d07c1181b3d2d11c678d50df4
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-07-15 16:59:55 +0400

    fixed all log in bugs

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,28 @@
 console.log('Hello from the parsely console!');
+/* eslint-disable */
+
+import '@babel/polyfill';
+import { login } from './login.js';
+
+// DOM
+const loginForm = document.querySelector('.form');
+
+// VALUES
+
+// document.querySelector('.form').addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const email = document.getElementById('email').value;
+//   const password = document.getElementById('password').value;
+
+//   login(email, password);
+// });
+
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    console.log(email, password);
+    login(email, password);
+  });
+}
