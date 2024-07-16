dfea48bda9c8e75d2a8d9124cf73396e753aa498
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-07-16 14:00:03 +0400

    finished implementing rendering error pages

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
@@ -17,16 +18,14 @@ exports.getTours = catchAsync(async (req, res, next) => {
     path: 'reviews',
     fields: 'review rating user',
   });
-  res
-    .status(200)
-    // .set(
-    //   'Content-Security-Policy',
-    //   "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;",
-    // )
-    .render('tour', {
-      title: `${tour.name} Tour`,
-      tour,
-    });
+  if (!tour) {
+    return next(new AppError('There is no tour with that name', 404));
+  }
+
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour,
+  });
 });
 
 exports.logIn = catchAsync(async (req, res, next) => {
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -13298,7 +13298,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40035" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45657" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
