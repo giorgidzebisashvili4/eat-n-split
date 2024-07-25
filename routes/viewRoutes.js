ab1752eb8717693d18bcb3907b6934c78d6d92e3
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-07-25 15:41:37 +0400

    implementing bookings api

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsController.getOverview,
+);
 router.get(
   '/tour/:slug',
   authController.isLoggedIn,
@@ -13,6 +19,7 @@ router.get(
 );
 router.get('/login', authController.isLoggedIn, viewsController.logIn);
 router.get('/me', authController.protect, viewsController.getAccount);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
 router.post(
   '/submit-user-data',
